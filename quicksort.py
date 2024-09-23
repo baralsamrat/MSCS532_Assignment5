be3cadf80e11458b66940f11b963bf98021ef84c
baralsamrat <sbaral30064@ucumberlands.edu>
2024-09-23 14:45:47 -0500

    Update quicksort.py & Added Report
    Generate Report and Added quicksort

--- quicksort.py
+++ quicksort.py
@@ -1,23 +1,40 @@
-def quicksort():
+import random
+import time
+
+def quicksort(arr):
     """
     Sorts an array using the deterministic Quicksort algorithm.
 
-    """
+    Parameters:
+        arr (list): The list of elements to sort.
 
-def randomized_quicksort():
+    Returns:
+        list: A new sorted list.
+    """
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]  # Middle = pivot
+    left = [x for x in arr if x < pivot]  # Elements < pivot
+    middle = [x for x in arr if x == pivot]  # Elements = pivot
+    right = [x for x in arr if x > pivot]  # Elements > pivot
+    return quicksort(left) + middle + quicksort(right)
+
+def randomized_quicksort(arr):
     """
     Sorts an array using the randomized Quicksort algorithm.
-
     """
 
-def performance_analysis():
+def performance_analysis(sort_function, arr):
     """
     Analyzes the performance of a sorting function.
-
     """
-   
 
 if __name__ == "__main__":
     # Testing the deterministic quicksort
+    sample_array = [3, 6, 8, 10, 1, 2, 1]
+    sorted_array = quicksort(sample_array)
+    print("Sorted array (deterministic):", sorted_array)
+
     # Testing the randomized quicksort
-    # Testing Performanc
+    # Testing  Performance
+
